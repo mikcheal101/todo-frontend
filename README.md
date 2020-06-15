a29858822f47e42543a1d637b46b8c20e9f0d871
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-15 20:27:11 +0100

    hemen/.

---
+++ README.md
@@ -0,0 +1,3 @@
+# todo-frontend
+This is a replica of the ubuntu 18-20.0 to do app.
+# todo-frontend
---
+++ src/errors/page404.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Page404 extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount = () => {
+    // set the state
+  };
+
+  render = () => {
+    return (
+      <>
+        <h4>404 - Page not found</h4>
+      </>
+    );
+  };
+}
+
+export default Page404;
